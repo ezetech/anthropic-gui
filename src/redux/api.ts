4c99500ed3c69c91d6e6323d2871dbee150668bb
Maksym Zorin <zorin.maksym@gmail.com>
2023-06-12 10:53:59 +0400

    feature/react-proxy

--- src/redux/api.ts
+++
@@ -1,8 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-import { BASE_URL } from '@/constants/anthropicApiUrl';
-
-export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
-  endpoints: () => ({}),
-});
