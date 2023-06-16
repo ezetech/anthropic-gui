6924f7cccea2ae62433be4a89e68dc8f0822b1ac
Pavlo <pavlo.yamchinsky@eze.tech>
2023-06-16 13:23:37 +0300

    feature: added chat logic

--- src/ui/IconComponent/IconComponent.tsx
+++ src/ui/IconComponent/IconComponent.tsx
@@ -1,19 +1,26 @@
 import { HTMLAttributes, memo } from 'react';
 
+import { ReactComponent as ai } from '@/assets/icons/ai.svg';
+import { ReactComponent as aiResponse } from '@/assets/icons/aiResponse.svg';
 import { ReactComponent as arrowDown } from '@/assets/icons/arrow-down.svg';
+import { ReactComponent as backgroundDefaultChat } from '@/assets/icons/backgroundDefaultChat.svg';
 import { ReactComponent as cancel } from '@/assets/icons/cancel.svg';
 import { ReactComponent as closedFolder } from '@/assets/icons/closed-folder.svg';
 import { ReactComponent as confirm } from '@/assets/icons/confirm.svg';
 import { ReactComponent as conversation } from '@/assets/icons/conversation.svg';
 import { ReactComponent as deleteIcon } from '@/assets/icons/delete.svg';
 import { ReactComponent as edit } from '@/assets/icons/edit.svg';
+import { ReactComponent as human } from '@/assets/icons/human.svg';
 import { ReactComponent as logoDark } from '@/assets/icons/logo-dark.svg';
 import { ReactComponent as logoLight } from '@/assets/icons/logo-light.svg';
 import { ReactComponent as logout } from '@/assets/icons/logout.svg';
 import { ReactComponent as newFolder } from '@/assets/icons/new-folder.svg';
 import { ReactComponent as openedFolder } from '@/assets/icons/opened-folder.svg';
 import { ReactComponent as plus } from '@/assets/icons/plus.svg';
+import { ReactComponent as regenerate } from '@/assets/icons/regenerate.svg';
+import { ReactComponent as remove } from '@/assets/icons/remove.svg';
 import { ReactComponent as search } from '@/assets/icons/search.svg';
+import { ReactComponent as submit } from '@/assets/icons/submit.svg';
 import { ReactComponent as themeDark } from '@/assets/icons/theme-dark.svg';
 import { ReactComponent as themeLight } from '@/assets/icons/theme-light.svg';
 
@@ -34,6 +41,13 @@ const ICONS = {
   deleteIcon,
   confirm,
   cancel,
+  ai,
+  aiResponse,
+  remove,
+  human,
+  backgroundDefaultChat,
+  regenerate,
+  submit,
 } as const;
 
 export type IconType = keyof typeof ICONS;
